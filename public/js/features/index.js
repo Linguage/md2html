d775833a9037cf977d64b9a5df5063c3de027165
PeakHan618 <peakhan618@outlook.com>
2025-06-04 21:51:14 +0800

    feat: 添加MD2HTML项目2025架构评估报告，优化代码组织和架构设计

--- public/js/features/index.js
+++ public/js/features/index.js
@@ -2,3 +2,5 @@
 export { default as TocGenerator } from './toc.js';
 export { default as TemplateManager } from './templates.js';
 export { default as Exporter } from './exporter.js';
+// TODO: 待创建examples.js后取消下面注释
+// export { default as ExamplesManager } from './examples.js';
